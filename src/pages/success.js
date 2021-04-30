0aa311893088b2c69c8ac32742c4e51444bd822a
Ricardo Do Vale <pataco80@gmail.com>
2021-04-30 14:07:28 +0200

    add success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -3,7 +3,16 @@ import Layout from '../components/Layout'
 import { Link } from 'gatsby'
 const Success = () => {
   return (
-    <h4>success page</h4>
+    <Layout>
+      <section className='success-page'>
+        <div className='page-center'>
+          <h2>Your subscription successfull</h2>
+          <Link to='/' className='btn'>
+            Back to homme
+          </Link>
+        </div>
+      </section>
+    </Layout>
   )
 }
 
