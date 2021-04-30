26f73e26d561ba5ac57fa8e105cf9cd354cf8466
Ricardo Do Vale <pataco80@gmail.com>
2021-04-30 13:41:22 +0200

    form configuration

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -3,24 +3,33 @@ import Layout from '../components/Layout'
 const NewsLetter = () => {
   return (
     <Layout>
-      <section className="newsletter-page">
-        <div className="page-center">
+      <section className='newsletter-page'>
+        <div className='page-center'>
           <h2>Get all the latest stories to your inbox</h2>
           <h4>I write to my friends every few weeks</h4>
-          <form action="post" className="contact-form">
+          <form
+            action='post'
+            className='contact-form'
+            name='newsletter'
+            netlify-honeypot='bot-field'
+            data-netlify='true'
+            action='/success'
+          >
+            <input type='hidden' name='form-name' value='newsletter' />
+            <input type='hidden' name='bot-field' />
             <input
-              type="text"
-              className="form-control"
-              name="name"
-              placeholder="Your Name"
+              type='text'
+              className='form-control'
+              name='name'
+              placeholder='Your Name'
             />
             <input
-              type="email"
-              className="form-control"
-              name="email"
-              placeholder="Your Email"
+              type='email'
+              className='form-control'
+              name='email'
+              placeholder='Your Email'
             />
-            <button className="btn submit-btn form-control">
+            <button className='btn submit-btn form-control'>
               Subscribe Now
             </button>
           </form>
